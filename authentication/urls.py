29d7e9a8b2fd5611b8ee47a05376eb7cb3bca8d8
jasem-elwaar <jassem.elwaar@gmail.com>
2022-01-19 21:56:49 +0100

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,9 +4,10 @@ from .views import *
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('register', csrf_exempt(RegistrationView.as_view()) , name="register"),
-    path('login', LoginView.as_view() , name="login"),
-    path('validate-username', csrf_exempt(UsernameValidationView.as_view()) , name="validateusername"),
+    path('register', csrf_exempt(RegistrationView.as_view()), name="register"),
+    path('login', LoginView.as_view(), name="login"),
+    path('logout', Logout.as_view(), name="logout"),
+    path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validateusername"),
     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name="validateemail"),
-    path('activate/<uidb64>/<token>', VerificationView.as_view(),  name="activate")
+    path('activate/<uidb64>/<token>', VerificationView.as_view(), name="activate"),
 ]
---
+++ userpreferences/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import os
+# to communicate with external  files
+import json
+# to understand json stucture
+from django.conf import settings
+
+
+# Create your views here.
+
+
+def index(request):
+    currency_data = []
+    file_path = os.path.join(settings.BASE_DIR, 'currencies.json')
+
+    with open(file_path, 'r') as json_file:
+        data = json.load(json_file)
+        for k, v in data.items():
+            currency_data.append({"name": k, 'value': v})
+
+    return render(request, 'preferences/index.html ', {'currencies': currency_data})
